2e0d3f8243ef05b115e5c457efb5a65e316177ac
SANTOSPECH <pechsantos52@gmail.com>
2025-02-13 10:58:41 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// app.js
+
+const math = require('./math'); // Importamos el módulo matemático
+
+const a = 10;
+const b = 5;
+
+console.log(`Suma: ${math.suma(a, b)}`);
+console.log(`Resta: ${math.resta(a, b)}`);
+console.log(`Multiplicación: ${math.multiplicacion(a, b)}`);
+console.log(`División: ${math.division(a, b)}`);
---
+++ index.js
@@ -0,0 +1,10 @@
+//Ejercicio 5
+//const imprimirEnMayusculas = (texto) => {
+   // console.log(texto.toUpperCase());
+//};
+
+// Ejemplo de uso
+//imprimirEnMayusculas("guillermo dzis"); // Imprime "HOLA MUNDO"
+//ejercicio 9
+// app.js
+
